3ff767bded14778ef1e71071c1775e1085907098
shaniashn <shaniashn76@gmail.com>
2024-07-18 14:26:09 +0800

    add initButtons

--- src/buttons.js
+++ src/buttons.js
@@ -1,5 +1,17 @@
 import { ICONS } from './constants';
 
-const toggleHighlighted = (icon, show) => document.querySelector(`${ICONS[icon]}-icon`).classList.toggle('highlighted', show);
+const toggleHighlighted = (icon, show) => {
+  // document.querySelector(`${ICONS[icon]}-icon`).classList.toggle('highlighted', show);
+}
 
-//comment
+export default function initButtons(handleUserAction) {
+  let selectedIcon = 0;
+
+  function buttonClicked({target}){
+    if (target.classList.contains("left-btn")){
+      toggleHighlighted(selectedIcon, false);
+      selectedIcon = (2 + selectedIcon) % ICONS.length;
+      toggleHighlighted(selectedIcon, true);
+    }
+  }
+}
